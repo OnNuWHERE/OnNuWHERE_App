633a4eace999563e4bc65b4fab22031ad3e089c3
camael407 <camael407@naver.com>
2023-01-31 12:34:50 +0900

    add use gps for current geo

--- app/src/main/java/com/example/onnuwhere/MainActivity.java
+++ app/src/main/java/com/example/onnuwhere/MainActivity.java
@@ -1,99 +1,210 @@
 package com.example.onnuwhere;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import android.content.Context;
-import android.content.pm.PackageInfo;
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Base64;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+public class MainActivity extends AppCompatActivity implements MapView.CurrentLocationEventListener, MapView.MapViewEventListener {
 
-public class MainActivity extends AppCompatActivity {
-    public MapView mMapView;
-    public MapPoint mapPoint;
-    public LocationManager lm;
+    String[] REQUIRED_PERMISSIONS  = {Manifest.permission.ACCESS_FINE_LOCATION};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getHashKey();
+//        getHashKey();
 
         MapView mapView = new MapView(MainActivity.this);
 
         ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
+        mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading);
+        if(!checkLocationServiceStatus()){
+            showDialogForGpsServiceSetting();
+        }else {
+            checkRunTimePermission();
+        }
+    }
 
-        Handler mHandler = new Handler();
-        mHandler.postDelayed(new Runnable() {
+
+
+//
+//    private void getHashKey() {
+//        PackageInfo packageInfo = null;
+//        try {
+//            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+//        } catch (PackageManager.NameNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//        if (packageInfo == null)
+//            Log.e("KeyHash", "KeyHash:null");
+//
+//        for (Signature signature : packageInfo.signatures) {
+//            try {
+//                MessageDigest md = MessageDigest.getInstance("SHA");
+//                md.update(signature.toByteArray());
+//                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+//            } catch (NoSuchAlgorithmException e) {
+//                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+//            }
+//        }
+//    }
+
+    @Override
+    public void onCurrentLocationUpdate(MapView mapView, MapPoint mapPoint, float v) {
+        MapPoint.GeoCoordinate mPointGeo = mapPoint.getMapPointGeoCoord();
+        Log.i("MainActivity", String.format("MapView onCurrentLocationUpdate (%f,%f) accuracy (%f)", mPointGeo.latitude, mPointGeo.longitude, v));
+    }
+
+    @Override
+    public void onCurrentLocationDeviceHeadingUpdate(MapView mapView, float v) {
+
+    }
+
+    @Override
+    public void onCurrentLocationUpdateFailed(MapView mapView) {
+
+    }
+
+    @Override
+    public void onCurrentLocationUpdateCancelled(MapView mapView) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==100 && grantResults.length==REQUIRED_PERMISSIONS.length){
+            boolean check_result = true;
+
+            for (int result : grantResults){
+                if(result != PackageManager.PERMISSION_GRANTED){
+                    check_result = false;
+                }
+            }
+            if(check_result){
+                Log.d("@@@", "start");
+            }else if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])){
+                Toast.makeText(this, "Reject Permission", Toast.LENGTH_SHORT).show();
+                finish();
+            }else {
+                Toast.makeText(this, "reject Permission", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    void checkRunTimePermission(){
+        //런타임 퍼미션 처리
+        //1. 위치 퍼미션을 가지고 있는지 확인
+        int hasFineLocationPermission = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if(hasFineLocationPermission == PackageManager.PERMISSION_GRANTED){
+            //이미 가지고 있다면 위치값을 가지고 올 수 있음
+        }else{
+            //퍼미션 허용 x 시 요청이 필요함
+            ActivityCompat.requestPermissions(MainActivity.this, REQUIRED_PERMISSIONS, 100);
+        }
+    }
+
+    private void  showDialogForGpsServiceSetting(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("위치 서비스 비활성화");
+        builder.setMessage("앱을 사용하기 위해서는 위치 서비스가 필요합니다.\n" + "위치 설정을 수정하시겠습니까?");
+        builder.setCancelable(true);
+        builder.setPositiveButton("설정", new DialogInterface.OnClickListener() {
             @Override
-            public void run() {
-                mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithHeading);
+            public void onClick(DialogInterface dialog, int which) {
+                Intent callGpsSettingIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivityForResult(callGpsSettingIntent, 2001);
             }
-        },4000);
-        lm = (LocationManager) getApplicationContext().getSystemService( Context.LOCATION_SERVICE );
-    }
-    public void Marker(String MakerName, double startX, double startY) {
-        mapPoint = MapPoint.mapPointWithGeoCoord( startY, startX );
-        mMapView.setMapCenterPoint( mapPoint, true );
-        //true면 앱 실행 시 애니메이션 효과가 나오고 false면 애니메이션이 나오지않음.
-        MapPOIItem marker = new MapPOIItem();
-        marker.setItemName(MakerName); // 마커 클릭 시 컨테이너에 담길 내용
-        marker.setMapPoint( mapPoint );
-        // 기본으로 제공하는 BluePin 마커 모양.
-        marker.setMarkerType( MapPOIItem.MarkerType.RedPin );
-        // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
-        marker.setSelectedMarkerType( MapPOIItem.MarkerType.BluePin );
-        mMapView.addPOIItem( marker );
-    }
-
-    public void MapMarker(String MakerName, String detail, double startX, double startY) {
-        mapPoint = MapPoint.mapPointWithGeoCoord( startY, startX );
-        mMapView.setMapCenterPoint( mapPoint, true );
-        //true면 앱 실행 시 애니메이션 효과가 나오고 false면 애니메이션이 나오지않음.
-        MapPOIItem marker = new MapPOIItem();
-        marker.setItemName(MakerName+"("+detail+")"); // 마커 클릭 시 컨테이너에 담길 내용
-        marker.setMapPoint( mapPoint );
-        // 기본으로 제공하는 BluePin 마커 모양.
-        marker.setMarkerType( MapPOIItem.MarkerType.RedPin );
-        // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
-        marker.setSelectedMarkerType( MapPOIItem.MarkerType.BluePin );
-        mMapView.addPOIItem( marker );
-    }
-
-
-
-    private void getHashKey() {
-        PackageInfo packageInfo = null;
-        try {
-            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        if (packageInfo == null)
-            Log.e("KeyHash", "KeyHash:null");
-
-        for (Signature signature : packageInfo.signatures) {
-            try {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-            } catch (NoSuchAlgorithmException e) {
-                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+        });
+        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
             }
+        });
+        builder.create().show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode){
+            case 2001: if(checkLocationServiceStatus()){
+                Log.d("@@@","활성화");
+                checkRunTimePermission();
+                return;
+            }
+            break;
         }
     }
 
+    public boolean checkLocationServiceStatus(){
+       LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+       return  locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+   }
+
+    @Override
+    public void onMapViewInitialized(MapView mapView) {
+
+    }
+
+    @Override
+    public void onMapViewCenterPointMoved(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewZoomLevelChanged(MapView mapView, int i) {
+
+    }
+
+    @Override
+    public void onMapViewSingleTapped(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDoubleTapped(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewLongPressed(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDragStarted(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDragEnded(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewMoveFinished(MapView mapView, MapPoint mapPoint) {
+
+    }
 }
