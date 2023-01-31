a67422d482f608f6ecab7da828f1c21e63af6848
camael407 <camael407@naver.com>
2023-01-31 11:26:09 +0900

    add splash screan

--- app/src/main/java/com/example/onnuwhere/MainActivity.java
+++ app/src/main/java/com/example/onnuwhere/MainActivity.java
@@ -2,33 +2,78 @@ package com.example.onnuwhere;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Base64;
 import android.util.Log;
 import android.view.ViewGroup;
 
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
+    public MapView mMapView;
+    public MapPoint mapPoint;
+    public LocationManager lm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getHashKey();
 
         MapView mapView = new MapView(MainActivity.this);
 
         ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
 
-        getHashKey();
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithHeading);
+            }
+        },4000);
+        lm = (LocationManager) getApplicationContext().getSystemService( Context.LOCATION_SERVICE );
     }
+    public void Marker(String MakerName, double startX, double startY) {
+        mapPoint = MapPoint.mapPointWithGeoCoord( startY, startX );
+        mMapView.setMapCenterPoint( mapPoint, true );
+        //true면 앱 실행 시 애니메이션 효과가 나오고 false면 애니메이션이 나오지않음.
+        MapPOIItem marker = new MapPOIItem();
+        marker.setItemName(MakerName); // 마커 클릭 시 컨테이너에 담길 내용
+        marker.setMapPoint( mapPoint );
+        // 기본으로 제공하는 BluePin 마커 모양.
+        marker.setMarkerType( MapPOIItem.MarkerType.RedPin );
+        // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+        marker.setSelectedMarkerType( MapPOIItem.MarkerType.BluePin );
+        mMapView.addPOIItem( marker );
+    }
+
+    public void MapMarker(String MakerName, String detail, double startX, double startY) {
+        mapPoint = MapPoint.mapPointWithGeoCoord( startY, startX );
+        mMapView.setMapCenterPoint( mapPoint, true );
+        //true면 앱 실행 시 애니메이션 효과가 나오고 false면 애니메이션이 나오지않음.
+        MapPOIItem marker = new MapPOIItem();
+        marker.setItemName(MakerName+"("+detail+")"); // 마커 클릭 시 컨테이너에 담길 내용
+        marker.setMapPoint( mapPoint );
+        // 기본으로 제공하는 BluePin 마커 모양.
+        marker.setMarkerType( MapPOIItem.MarkerType.RedPin );
+        // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+        marker.setSelectedMarkerType( MapPOIItem.MarkerType.BluePin );
+        mMapView.addPOIItem( marker );
+    }
+
+
 
     private void getHashKey() {
         PackageInfo packageInfo = null;
